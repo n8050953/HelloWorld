55053fbd56c99a116938ab713d20590cd1ef1e67
Minh Tam <Minh Tam@MinhTam.BigPond>
2012-08-18 00:32:45 +1000

    Changed output message

--- tam.git.first/src/tam/git/first/GitTest.java
+++ tam.git.first/src/tam/git/first/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Fuck this bullshit");
+		System.out.println("Fuck this shit");
 	}
 
 }
